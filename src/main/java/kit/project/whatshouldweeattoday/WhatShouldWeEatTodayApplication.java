aea5a42b24f6e8df384c6413dd4c2424ca7e205b
Jihyun3478 <leejihyun3478@gmail.com>
2024-03-19 16:38:16 +0900

    test

---
+++ src/main/java/kit/project/whatshouldweeattoday/WhatShouldWeEatTodayApplication.java
@@ -0,0 +1,13 @@
+package kit.project.whatshouldweeattoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatShouldWeEatTodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatShouldWeEatTodayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kit/project/whatshouldweeattoday/WhatShouldWeEatTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package kit.project.whatshouldweeattoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatShouldWeEatTodayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
