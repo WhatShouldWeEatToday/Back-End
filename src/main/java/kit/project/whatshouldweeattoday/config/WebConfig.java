14bc65c02116da228928d1a2d4ea6b0473f66836
leesorim <solimsa11@naver.com>
2024-04-20 20:38:57 +0900

    [0420] 맛집 조회 기능 백엔드 코드 추가 [프론트와 기능 테스트 완료]

---
+++ src/main/java/kit/project/whatshouldweeattoday/config/WebConfig.java
@@ -0,0 +1,29 @@
+package kit.project.whatshouldweeattoday.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .allowedMethods("OPTIONS", "GET", "POST", "PATCH", "DELETE")
+                .allowCredentials(true);
+    }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("/webjars/")
+//                .resourceChain(false);
+//        registry.setOrder(1);
+//    }
+}
---
+++ src/main/java/kit/project/whatshouldweeattoday/controller/RestaurantController.java
@@ -0,0 +1,73 @@
+package kit.project.whatshouldweeattoday.controller;
+
+import kit.project.whatshouldweeattoday.domain.dto.restaurant.RestaurantResponseDTO;
+import kit.project.whatshouldweeattoday.domain.dto.user.signup.SignupRequestDTO;
+import kit.project.whatshouldweeattoday.domain.dto.user.signup.SignupResponseDTO;
+import kit.project.whatshouldweeattoday.domain.entity.Restaurant;
+import kit.project.whatshouldweeattoday.service.RestaurantService;
+import kit.project.whatshouldweeattoday.service.TMapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/restaurant")
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @GetMapping("/search/")
+    public ResponseEntity<Page<RestaurantResponseDTO>> search(String word,
+                                                              @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 10) Pageable pageable) {
+        Page<RestaurantResponseDTO> page = restaurantService.searchRestaurants(word, pageable);
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+
+    @GetMapping("/findAll/")
+    public ResponseEntity<Page<RestaurantResponseDTO>> findAll(
+                                                              @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 10) Pageable pageable) {
+        System.out.println("findAll 함수 들어옴");
+        Page<RestaurantResponseDTO> page = restaurantService.findAll(pageable);
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+
+    //맛집 조회(리뷰평점순(degree))
+    @GetMapping("/search/degree/")
+    public ResponseEntity<Page<RestaurantResponseDTO>> getRestaurantsByDegree(String word,
+                                                                              @PageableDefault(sort = "degree", direction = Sort.Direction.DESC, size = 10)Pageable pageable ){
+        Page<RestaurantResponseDTO> page = restaurantService.searchRestaurants(word, pageable);
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+
+    //맛집 조회(리뷰개수순(total_reviews))
+    @GetMapping("/search/reviews/")
+    public ResponseEntity<Page<RestaurantResponseDTO>> getRestaurantsByTotalReviews(String word,
+                                                                                    @PageableDefault(sort = "totalReviews", direction = Sort.Direction.DESC, size = 15)Pageable pageable ){
+        Page<RestaurantResponseDTO> page = restaurantService.searchRestaurants(word, pageable);
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+
+    //맛집조회(음식점만)
+    @GetMapping("/search/onlyrestaurants/")
+    public ResponseEntity<Page<RestaurantResponseDTO>> findOnlyRestaurant(String word,
+                                                                          @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 10) Pageable pageable){
+        Page<RestaurantResponseDTO> page = restaurantService.searchOnlyRestaurant(word, pageable);
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+
+
+    //맛집조회(카페만) ==> 카페,제과,베이커리,케이크,커피
+    @GetMapping("/search/onlycafes/")
+    public ResponseEntity<Page<RestaurantResponseDTO>> findOnlyCafe(String word,
+                                                                    @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 10) Pageable pageable){
+        Page<RestaurantResponseDTO> page = restaurantService.searchOnlyCafes(word, pageable);
+        return new ResponseEntity<>(page, HttpStatus.OK);
+    }
+}
+
