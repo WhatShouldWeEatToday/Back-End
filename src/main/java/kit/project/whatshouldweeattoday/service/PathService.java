b59bd46d971d0a527791e782b4bf078683e926f5
leesorim <solimsa11@naver.com>
2024-05-23 15:50:13 +0900

    [0523] 단체경로함수 수정중

--- src/main/java/kit/project/whatshouldweeattoday/service/PathService.java
+++ src/main/java/kit/project/whatshouldweeattoday/service/PathService.java
@@ -19,53 +19,56 @@ public class PathService {
     private int serialNum = 0; // 음식점 배열 일련번호
     private List<PersonalPath> weightArray = new ArrayList<>(); // 전체 가중치
 
-    // num = 채팅방사람들 수, startAddres = 채팅방 사람들의 출발지list
+    // num = 채팅방사람들 수, startAddres = 채팅방 사람들의 출발지 list
     @Transactional
     public List<PersonalPath> getWeight(String keyword, List<String> startAddress, String searchDttm) {
         List<PersonalPath> resultSort = new ArrayList<>(); //ex) A와 B와 C에 대해서 나온 것들을 순차적으로 저장한 배열 -> 시리얼 넘버랑, 각 식당에 대해서 가지고 있음
-        for (int i = 0; i < startAddress.size(); i++) { //allPathList를 채워줌
-            String targetAddress = startAddress.get(i);
+        for (String targetAddress : startAddress) { //allPathList를 채워줌
             List<PersonalPath> pathList = personalRestaurant(keyword, targetAddress, searchDttm);
             pathList = setSerialNumForArray(pathList);
             resultSort.addAll(pathList);
         }
-        for (int i = 0; i < startAddress.size(); i++) {
-            System.out.println("유저: " + startAddress.get(i));
-            Map<String, Double> coordinates = tmapService.getCoordinates(startAddress.get(i));
+        for (String address : startAddress) {
+            System.out.println("유저: " + address);
+            Map<String, Double> coordinates = tmapService.getCoordinates(address);
             Double startX = coordinates.get("longitude");
             Double startY = coordinates.get("latitude");
             List<PersonalPath> pathList = setTotalTime(resultSort, startX, startY, searchDttm);
             pathList = sortByPath(pathList);
-            for(int j = 0; j< pathList.size(); j++) {
+            for (int j = 0; j < pathList.size(); j++) {
                 PersonalPath target = pathList.get(j);
-                target.setWeight(target.getWeight()+j); //TODO 결과를 봤을 때 weight = 0 인 경우에 대해서 디버그를 통해서 하나씩 추적하기.
+                target.setWeight(target.getWeight() + j); //TODO weight = 0 인 경우확인
             }
         }
-        sortPersonalPathByWeight(resultSort);
+        sortPersonalPathByWeightTop3(resultSort);
 
         return resultSort;
     }
+
+    @Transactional
     public void sortPersonalPathByWeight(List<PersonalPath> list) {
-        Collections.sort(list, new Comparator<PersonalPath>() {
-            @Override
-            public int compare(PersonalPath p1, PersonalPath p2) {
-                return Integer.compare(p1.getWeight(), p2.getWeight());
-            }
-        });
+        Collections.sort(list, Comparator.comparingInt(PersonalPath::getWeight));
     }
 
+    //최종 3개 추출
+    @Transactional
+    public List<PersonalPath> sortPersonalPathByWeightTop3(List<PersonalPath> list) {
+        // 리스트를 가중치 기준으로 정렬
+        Collections.sort(list, Comparator.comparingInt(PersonalPath::getWeight));
+
+        // 상위 3개의 항목을 반환
+        return list.size() > 3 ? list.subList(0, 3) : list;
+    }
+    @Transactional
     public List<PersonalPath> setTotalTime(List<PersonalPath> list, Double startX, Double startY, String searchDttm) {
         List<PersonalPath> results = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            PersonalPath target = list.get(i);
+        for (PersonalPath target : list) {
             target.setTotalTime(tmapService.totalTime(Double.toString(startX), Double.toString(startY),
-                    Double.toString(target.getRestaurant().getLongitude()), Double.toString(target.getRestaurant().getLatitude())
-            ,0, "json", 10, searchDttm));
+                    Double.toString(target.getRestaurant().getLongitude()), Double.toString(target.getRestaurant().getLatitude()), 0, "json", 1, searchDttm));
             results.add(target);
         }
         return results;
     }
-
 
     //1차 배열
     @Transactional
@@ -80,7 +83,7 @@ public class PathService {
         //2. 사용자의 위치정보를 주소로 반환후 XX동 XX까지 추출
         String userAddress = tmapService.getAddressByCoordinates(startX, startY);
         //3. 주소 & 키워드로 음식점 검색
-        restaurants = restaurantRepository.findByKeywordAndAddress(keyword,userAddress);
+        restaurants = restaurantRepository.findByKeywordAndAddress(keyword, userAddress);
         //4.리뷰평점순으로 20개 추출(1차 필터링)
         restaurants = sortByDegree(restaurants);
         //5. 경로구하기 -> 여기서 personalPath로 변환
@@ -93,8 +96,8 @@ public class PathService {
     //2차 배열 -> 일련번호 넣어주기(쓰레드 공유 이슈 생길 수 있음..), 다른사람도 serialNum값을 사용함
     @Transactional
     public List<PersonalPath> setSerialNumForArray(List<PersonalPath> personalPaths) {
-        for (int i = 0; i < personalPaths.size(); i++) {
-            personalPaths.get(i).setSerialNum(serialNum);
+        for (PersonalPath personalPath : personalPaths) {
+            personalPath.setSerialNum(serialNum);
             serialNum++;
         }
         return personalPaths;
@@ -117,11 +120,11 @@ public class PathService {
                 .collect(Collectors.toList());
     }
 
-    //경로순으로 정렬
+    //경로순으로 정렬 -> 오름차순
     @Transactional
     public List<PersonalPath> sortByPath(List<PersonalPath> personalPaths) {
         return personalPaths.stream()
-                .sorted((r1, r2) -> Double.compare(r2.getTotalTime(), r1.getTotalTime()))
+                .sorted((r1, r2) -> Double.compare(r1.getTotalTime(), r2.getTotalTime()))
                 .limit(5)
                 .collect(Collectors.toList());
     }
@@ -130,18 +133,15 @@ public class PathService {
     @Transactional
     public List<PersonalPath> getPath(Double startX, Double startY, List<Restaurant> restaurants, String searchDttm) {
         List<PersonalPath> personalPaths = new ArrayList<>();
-       /* for (int i = 0; i < restaurants.size(); i++) {
-            restaurants.get(i).setPathTime(tmapService.totalTime(Double.toString(startX), Double.toString(startY),
-                    Double.toString(restaurants.get(i).getLongitude()), Double.toString(restaurants.get(i).getLatitude()), 0, "json", 10, searchDttm));
-        }*/
-        for (int i = 0; i < restaurants.size(); i++) {
+        for (Restaurant restaurant : restaurants) {
             PersonalPath target = new PersonalPath();
             target.setTotalTime(tmapService.totalTime(Double.toString(startX), Double.toString(startY),
-                    Double.toString(restaurants.get(i).getLongitude()), Double.toString(restaurants.get(i).getLatitude()), 0, "json", 10, searchDttm));
-            target.setRestaurant(restaurants.get(i));
+                    Double.toString(restaurant.getLongitude()), Double.toString(restaurant.getLatitude()), 0, "json", 10, searchDttm));
+            target.setRestaurant(restaurant);
             personalPaths.add(target);
         }
         return personalPaths;
     }
 
 }
+
