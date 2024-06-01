7ca6f77551e1033c51036bf878385e97267c6f43
leesorim <solimsa11@naver.com>
2024-06-01 21:33:37 +0900

    [0601] 좋아요 수정4

--- src/main/java/kit/project/whatshouldweeattoday/domain/dto/likes/LikesRequestDTO.java
+++ src/main/java/kit/project/whatshouldweeattoday/domain/dto/likes/LikesRequestDTO.java
@@ -1,6 +1,7 @@
 package kit.project.whatshouldweeattoday.domain.dto.likes;
 
 import kit.project.whatshouldweeattoday.domain.entity.Likes;
+import kit.project.whatshouldweeattoday.domain.entity.Member;
 import kit.project.whatshouldweeattoday.domain.entity.Review;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,9 +21,11 @@ public class LikesRequestDTO {
         this.review=review;
         this.state=state;
     }
-    public Likes toSaveEntity(){
+    public Likes toSaveEntity(Member member, Review review){
         System.out.println("좋아요 테이블에 리뷰 저장");
         return Likes.builder()
+                .member(member)
+                .review(review)
                 .state(state)
                 .build();
     }
