0713f1a18d2af96889b2bf6a4ae43225b463f6dc
leesorim <solimsa11@naver.com>
2024-06-03 01:56:29 +0900

    [0603] 경로함수 오류 수정

--- src/main/java/kit/project/whatshouldweeattoday/domain/dto/restaurant/PathRequestDTO.java
+++ src/main/java/kit/project/whatshouldweeattoday/domain/dto/restaurant/PathRequestDTO.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class PathRequestDTO {
     private String departure;
     private String destination;
+    private List<String> destinations;
     private String searchDttm; //출발예정시각
 }
